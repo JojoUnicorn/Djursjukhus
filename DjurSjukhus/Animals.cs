47af51da6624460c632e9b4df70a79113384aa02
Joanna <joannita97@hotmail.com>
2024-12-09 17:01:10 +0100

    new

---
+++ DjurSjukhus/Animals.cs
@@ -0,0 +1,33 @@
+﻿
+namespace DjurSjukhus
+{
+    public class Animals
+    {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Age { get; set; }
+        public bool IsVaccinated { get; set; }
+        public int NumberOfLegs { get; set; }
+
+
+        public Animals(string name, string species, int age, bool isVaccinated, int numberOfLegs)
+        {
+            Species = species;
+            Name = name;
+            Age = age;
+            IsVaccinated = isVaccinated;
+            NumberOfLegs = numberOfLegs;
+        }
+
+        //Method for information about the animals
+        public void Info()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Species: {Species}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Vaccinated: {IsVaccinated}");
+            Console.WriteLine($"Number of Legs: {NumberOfLegs}");
+        }
+    }
+}
+
---
+++ DjurSjukhus/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace DjurSjukhus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                List<Animals> animal = new List<Animals>
+                {
+                    new Animals("Ponyo", "Cat", 2, true, 4),
+                    new Animals("Shadow", "Dog", 4, true, 4),
+                    new Animals("Jetson", "Dog", 3, false, 6),
+                    new Animals("SnooFlayk", "Cat", 6, false, 2),
+                    new Animals("Black Noir", "Spider", 55, true, 54),
+                    new Animals("Don Julio", "Goat", 5, false, 4),
+                    new Animals("Manolo", "Dog", 14, false, 4),
+                    new Animals("Thing", "Cat", 12, true, 5),
+                    new Animals("Exterminator", "Hamster", 5, true, 6),
+                    new Animals("Pan", "Cat", 9, true, 3),
+                    new Animals("Dog", "Dog", 2, true, 4)
+                };
+
+                // How many dogs
+
+                var NumberOfDogs = animal.Count
+                (a => a.Species == "Dog");
+                Console.WriteLine($"How many dogs: {NumberOfDogs}");
+                Console.WriteLine("---------------");
+                // Oldest animal
+
+                var oldestAnimal = animal
+                    .OrderByDescending(a => a.Age)
+                    .FirstOrDefault();
+                Console.WriteLine($"Oldest Animal: ");
+                oldestAnimal?.Info();
+                Console.WriteLine("------------------");
+
+                // Vaccinated
+                var VaccinatedAnimals = animal
+                    .Where(a => a.IsVaccinated).ToList();
+                Console.WriteLine("Vaccinated animals: ");
+                foreach (var a in VaccinatedAnimals)
+                {
+                    Console.WriteLine($"'{a.Name}', {a.Species}.");
+                }
+                Console.WriteLine("-------------------");
+
+                // 4-legged animal that is older than 3
+                var FourLegsOlderThanThree = animal
+                    .Where(a => a.NumberOfLegs == 4 && a.Age > 3).ToList();
+                Console.WriteLine("4-Legged animals older than 3 years old: ");
+                foreach (var a in FourLegsOlderThanThree)
+                {
+                    Console.WriteLine($"'{a.Name}', {a.Age} Years.");
+                }
+                Console.WriteLine("------------------");
+
+                //Is there an animal called "Shadow"?
+
+                var IsShadow = animal
+                    .Any(a => a.Name == "Shadow");
+                Console.WriteLine($"Animal named 'Shadow': {IsShadow}.");
+                Console.WriteLine("------------------");
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}   
